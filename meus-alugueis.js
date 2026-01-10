0b20e4b5c05516cfbdd5b6695afd33b26f7b759c
Felipe Medeiros <81656960+feliphemedeiros@users.noreply.github.com>
2026-01-10 19:54:17 -0300

    Add files via upload

---
+++ meus-alugueis.js
@@ -0,0 +1,30 @@
+const rents = [
+    { mes: "Fevereiro / 2026", venceEm: 31, valor: "R$ 850,00" },
+    { mes: "Março / 2026", venceEm: 59, valor: "R$ 850,00" },
+    { mes: "Abril / 2026", venceEm: 90, valor: "R$ 850,00" },
+    { mes: "Maio / 2026", venceEm: 121, valor: "R$ 850,00" },
+    { mes: "Junho / 2026", venceEm: 151, valor: "R$ 850,00" },
+    { mes: "Julho / 2026", venceEm: 181, valor: "R$ 850,00" }
+];
+
+const list = document.getElementById("rentList");
+
+rents.forEach(item => {
+    const card = document.createElement("div");
+    card.className = "rent-card";
+
+    card.innerHTML = `
+        <div class="rent-info">
+            <strong>${item.mes}</strong>
+            <span class="status-open">🔵 Em aberto</span>
+            <span>Vence em ${item.venceEm} dia(s)</span>
+        </div>
+
+        <div class="rent-action">
+            <span class="rent-value">${item.valor}</span>
+            <button class="pay-btn">Pagar PIX</button>
+        </div>
+    `;
+
+    list.appendChild(card);
+});
